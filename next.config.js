414bf2ae7871bed45fa42b3762fa4536b8c95da1
Pierre Bertet <hi@bpier.re>
2023-03-20 13:59:33 +0000

    better URLs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [{ source: "/:id", destination: "/api/og?promise=:id" }];
+  },
 };
 
 module.exports = nextConfig;
