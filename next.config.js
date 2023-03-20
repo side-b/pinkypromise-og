48052aaec7985a75204711cc9460578448eba091
Pierre Bertet <hi@bpier.re>
2023-03-20 01:31:06 +0000

    fix build

--- next.config.js
+++ next.config.js
@@ -1,20 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, { webpack }) => {
-    config.plugins.push(
-      // imported by viem => noble-hashes
-      new webpack.NormalModuleReplacementPlugin(/node:/, (resource) => {
-        const mod = resource.request.replace(/^node:/, "");
-        switch (mod) {
-          case "crypto":
-            resource.request = "buffer";
-            break;
-          default:
-            throw new Error(`Not found ${mod}`);
-        }
-      }),
-    );
+  webpack: (config, { webpack, dev }) => {
+    // "node:crypto": is imported by viem => noble-hashes
+    if (dev) {
+      config.plugins.push(
+        new webpack.NormalModuleReplacementPlugin(/node:/, (resource) => {
+          const mod = resource.request.replace(/^node:/, "");
+          switch (mod) {
+            case "crypto":
+              resource.request = "buffer";
+              break;
+            default:
+              throw new Error(`Not found ${mod}`);
+          }
+        }),
+      );
+    } else {
+      config.plugins.push(
+        new webpack.IgnorePlugin({
+          resourceRegExp: /^\@noble\/hashes\/crypto$/,
+        }),
+        new webpack.IgnorePlugin({ resourceRegExp: /^crypto$/ }),
+      );
+    }
     return config;
   },
 };
